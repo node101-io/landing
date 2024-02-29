9372bf2b6028c6b6bb98b0f0db97cb78cda7d618
Yunus Gürlek <yunusguerlek@gmail.com>
2024-02-29 15:26:54 +0100

    Fix header scroll animation

--- public/js/functions/header.js
+++ public/js/functions/header.js
@@ -2,7 +2,7 @@ let headerIsLanguageButtonOpen = false;
 
 let HEADER_HEIGHT;
 
-function headerBackgroundAnimation(percentage) {
+function headerBackgroundAnimation(percentage, amount) {
   if (typeof percentage != 'number' || percentage < 0 || percentage > 1)
     return;
 
@@ -10,12 +10,24 @@ function headerBackgroundAnimation(percentage) {
   const buttonsWrapper = document.querySelector('.header-buttons-wrapper');
   const headerMenuWrapper = document.querySelector('.header-menu-wrapper');
 
+  const startPageLeftColor = document.querySelector('.start-page-left-color');
+  const startPageRightColor = document.querySelector('.start-page-right-color');
+
   headerLogoWrapper.style.borderBottomRightRadius = `calc((var(--header-height) - 10px) * ${1 - percentage})`;
   buttonsWrapper.style.height = `calc(calc(var(--header-height) - 2 * ${1 - percentage} * var(--header-vertical-padding)))`;
-  buttonsWrapper.style.borderRadius = `calc(((var(--header-height) - 2 * var(--header-vertical-padding)) / 2) * ${1 - percentage})`;
+  buttonsWrapper.style.borderBottomLeftRadius = buttonsWrapper.style.borderBottomRightRadius = `calc(((var(--header-height) - 2 * var(--header-vertical-padding)) / 2) * ${1 - percentage})`;
+  buttonsWrapper.style.borderTopLeftRadius = buttonsWrapper.style.borderTopRightRadius = `calc(max(var(--header-vertical-padding), ((var(--header-height) - 2 * var(--header-vertical-padding)) / 2) * ${1 - percentage}))`;
   buttonsWrapper.style.width = `calc(100vw - 2 * var(--page-horizontal-padding) - 3 * var(--header-horizontal-padding) - 2 * ${(1 - percentage)} * var(--header-gap) - var(--header-logo-width) - (2 * var(--header-menu-gap) + var(--header-stake-with-node101-button-width) + var(--header-change-language-button-width)))`;
   buttonsWrapper.style.minWidth = `calc(100vw - 2 * var(--page-horizontal-padding) - 3 * var(--header-horizontal-padding) - 2 * ${(1 - percentage)} * var(--header-gap) - var(--header-logo-width) - (2 * var(--header-menu-gap) + var(--header-stake-with-node101-button-width) + var(--header-change-language-button-width)))`;
+  buttonsWrapper.style.padding = `0 calc(var(--header-gap) * ${percentage})`;
   headerMenuWrapper.style.borderBottomLeftRadius = `calc((var(--header-height) - 10px) * ${1 - percentage})`;
+
+  if (startPageLeftColor && startPageRightColor) {
+    startPageLeftColor.style.height = `calc(max(0px, (100vh - 2 * var(--page-vertical-padding) - var(--header-height)) - ${amount}px)`;
+    startPageLeftColor.style.minHeight = `calc(max(0px, (100vh - 2 * var(--page-vertical-padding) - var(--header-height)) - ${amount}px)`;
+    startPageRightColor.style.height = `calc(max(0px, (100vh - 2 * var(--page-vertical-padding) - var(--header-height)) - ${amount}px)`;
+    startPageRightColor.style.minHeight = `calc(max(0px, (100vh - 2 * var(--page-vertical-padding) - var(--header-height)) - ${amount}px)`;
+  }
 };
 
 window.addEventListener('load', _ => {
@@ -43,6 +55,6 @@ window.addEventListener('load', _ => {
 
   document.querySelector('.content-wrapper').addEventListener('scroll', event => {
     const percentage = Math.min(1, event.target.scrollTop / HEADER_HEIGHT);
-    headerBackgroundAnimation(percentage);
+    headerBackgroundAnimation(percentage, event.target.scrollTop);
   });
 });
