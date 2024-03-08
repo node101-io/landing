f62625aac23418ec7d22de452a93dc5cd7baaf3e
Yunus Gürlek <yunusguerlek@gmail.com>
2024-03-08 17:36:50 +0100

    Add services and improve header

--- controllers/index/get.js
+++ controllers/index/get.js
@@ -1,89 +1,133 @@
-const PORTFOLIO = [
-  {
-    _id: 'asset-mantle',
-    name: 'Asset Mantle'
-  },
-  {
-    _id: 'band-protocol',
-    name: 'Band Protocol'
-  },
-  {
-    _id: 'canto',
-    name: 'Canto'
-  },
-  {
-    _id: 'cheqd',
-    name: 'Cheqd'
-  },
-  {
-    _id: 'cosmos-hub',
-    name: 'Cosmos Hub'
-  },
-  {
-    _id: 'crescent',
-    name: 'Crescent'
-  },
-  {
-    _id: 'desmos',
-    name: 'Desmos'
-  },
-  {
-    _id: 'e-money',
-    name: 'E-Money'
-  },
-  {
-    _id: 'iris-net',
-    name: 'Iris Network'
-  },
-  {
-    _id: 'ki-chain',
-    name: 'Ki Chain'
-  },
-  {
-    _id: 'nym',
-    name: 'NYM Mixnode'
-  },
-  {
-    _id: 'orai-chain',
-    name: 'Orai Chain'
-  },
-  {
-    _id: 'shentu',
-    name: 'Shentu'
-  },
-  {
-    _id: 'umee',
-    name: 'UMEE'
-  }
-];
-const SERVICES = [
-  {
-    title: '100% Uptime Guarantee',
-    description: 'node101 has a 100% uptime guarantee in your stakes. You are compensated for any loss that originated from our actions. We are very confident that we will never miss a block, but isn’t it very reassuring to know that it is impossible to lose with node101?'
-  },
-  {
-    title: 'Auto Restaking',
-    description: 'By using the trustless auto restake tool we provide, you can maximize your gain with compound reward mechanisms. We restake the tokens you receive periodically and help you make most of your regular income.'
-  },
-  {
-    title: '0 Fee Auto Stake',
-    description: 'When you choose to auto restake with node101, you do not need to worry about the additional transaction fees coming from restaking orders. We take care of them for you. Why? Because you deserve the best.'
-  },
-  {
-    title: 'Notification Services',
-    description: 'Just stake your coins and forget about them… Not always a good idea, right? With the node101 newsletter, you can get the latest news about the chains you chose weekly. You can even add your public keys to track your gain! Wouldn’t it be nice to read about your investments sunday morning with a cup of coffee?'
-  },
-  {
-    title: 'One Click Staking',
-    description: 'Hundreds of chains, tens of wallets, and a lot to learn just to stake in a single chain… node101 provides you the option to stake in all chains we validate in only with a click. Just hit the “stake with us” button, stake now.'
-  },
-  {
-    title: '7/24 Support',
-    description: 'We do not see blockchain as just about a technology, but about people itself. Our support team is always there to help you, in any way you need with your blockchain questions. Just reach out at hello@node101.io. We are waiting to hear from you.'
-  }
-];
-
 module.exports = (req, res) => {
+  const PORTFOLIO = [
+    {
+      _id: 'asset-mantle',
+      name: 'Asset Mantle'
+    },
+    {
+      _id: 'band-protocol',
+      name: 'Band Protocol'
+    },
+    {
+      _id: 'canto',
+      name: 'Canto'
+    },
+    {
+      _id: 'cheqd',
+      name: 'Cheqd'
+    },
+    {
+      _id: 'cosmos-hub',
+      name: 'Cosmos Hub'
+    },
+    {
+      _id: 'crescent',
+      name: 'Crescent'
+    },
+    {
+      _id: 'desmos',
+      name: 'Desmos'
+    },
+    {
+      _id: 'e-money',
+      name: 'E-Money'
+    },
+    {
+      _id: 'iris-net',
+      name: 'Iris Network'
+    },
+    {
+      _id: 'ki-chain',
+      name: 'Ki Chain'
+    },
+    {
+      _id: 'nym',
+      name: 'NYM Mixnode'
+    },
+    {
+      _id: 'orai-chain',
+      name: 'Orai Chain'
+    },
+    {
+      _id: 'shentu',
+      name: 'Shentu'
+    },
+    {
+      _id: 'umee',
+      name: 'UMEE'
+    }
+  ];
+  const SERVICES = [
+    {
+      title: [res.__('100%'), res.__('Uptime Guarantee')],
+      description: [{
+        type: 'text',
+        content: res.__('We have 100% uptime guarantee in your stakes. You are compensated for any loss that originated from our actions. We are very confident that we will never miss a block, but isn’t it reassuring to know that it’s impossible to lose with node101?')
+      }]
+    },
+    {
+      title:[res.__('Auto'), res.__('Restaking')],
+      description: [{
+        type: 'text',
+        content: res.__('By using the trustless auto restake tool we provide, you can maximize your gain with compound reward mechanisms. We restake the tokens you receive periodically and help you make most of your regular income.')
+      }]
+    },
+    {
+      title: [res.__('0 Fee'), res.__('Auto Stake')],
+      description: [{
+        type: 'text',
+        content: res.__('When you choose to auto restake with node101, you do not need to worry about the additional transaction fees coming from restaking orders. We take care of them for you. Why? Because you deserve the best.')
+      }]
+    },
+    {
+      title: [res.__('Notification'), res.__('Services')],
+      description: [{
+        type: 'text',
+        content: res.__('Just stake and forget about your coins… Not always a good idea, right? With node101 newsletter, you can get the latest news weekly. Wouldn’t it be nice to read about your investments sunday morning with a cup of coffee?')
+      }]
+    },
+    {
+      title: [res.__('One Click'), res.__('Staking')],
+      description: [
+        {
+          type: 'text',
+          content: res.__('Hundreds of chains, tens of wallets, and a lot to learn just to stake in a single chain… We maximize your staking experience with our interface, allowing you to stake with just a click. Just hit the “stake with us” button,'),
+          spaceRight: true
+        },
+        {
+          type: 'button',
+          url: '/stake-with-node101',
+          target: '_self',
+          content: res.__('stake now')
+        },
+        {
+          type: 'text',
+          content: '.'
+        }
+      ]
+    },
+    {
+      title: [res.__('7/24'), res.__('Support')],
+      description: [
+        {
+          type: 'text',
+          content: res.__('We do not see blockchain as just about a technology, but about people itself. Our team is always here to help, in any way you need with your blockchain questions. Just reach out at'),
+          spaceRight: true
+        },
+        {
+          type: 'link',
+          url: 'mailto:hello@node101.io',
+          target: '_blank',
+          content: 'hello@node101.io'
+        },
+        {
+          type: 'text',
+          content: res.__('. We are waiting to hear from you.')
+        }
+      ]
+    }
+  ];
+
   return res.render('index/index', {
     page: 'index/index',
     title: res.__('For you to make most of the distributed value'),
@@ -100,7 +144,7 @@ module.exports = (req, res) => {
       }
     },
     portfolio: PORTFOLIO,
-    services: [],
+    services: SERVICES,
     events_title: '',
     events: [],
     contributions: [],
