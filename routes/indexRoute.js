11b669737d290a7a9f95a69ccd9914a88f5dc199
necipsagiro <sagiroglun22@itu.edu.tr>
2024-06-01 01:24:18 +0300

    feat: Add /stake route for stake information

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,13 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 const indexGetController = require('../controllers/index/get');
+const stakeGetController = require('../controllers/stake/get');
 const aboutUsGetController = require('../controllers/about-us/get');
 
 router.get(
   '/',
     indexGetController
 );
-
+router.get(
+  '/stake',
+    stakeGetController
+);
 router.get(
   '/about-us',
     aboutUsGetController
