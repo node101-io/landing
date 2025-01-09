1e5fb97291ecb2a600583f07ed1faa7fe5dfac39
necipsagiro <sagiroglun22@itu.edu.tr>
2025-01-09 18:33:46 +0300

    add minification for js

--- utils/minify.mjs
+++ utils/minify.mjs
@@ -1,6 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { minify } from 'csso';
+import swc from '@swc/core';
 
 const minifyCSS = (source) => {
   fs.readdir(source)
@@ -25,4 +26,30 @@ const minifyCSS = (source) => {
     });
 };
 
+const minifyJS = (source) => {
+  fs.readdir(source)
+    .then((folders) => {
+      folders.forEach((folder) => {
+        fs.readdir(path.join(source, folder))
+          .then((files) => {
+            files.forEach((file) => {
+              if (file.endsWith('.min.js')) return;
+
+              fs.readFile(path.join(source, folder, file), 'utf-8')
+                .then((data) => {
+                  const minified = swc.minifySync(data).code;
+
+                  fs.writeFile(
+                    path.join(source, folder, file.replace('.js', '.min.js')),
+                    minified
+                  );
+                });
+            });
+          });
+      });
+    });
+
+};
+
 minifyCSS(path.join(import.meta.dirname, '../public/css'));
+minifyJS(path.join(import.meta.dirname, '../public/js'));
