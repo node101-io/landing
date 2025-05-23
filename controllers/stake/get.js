0b82588bb29e8b92780ca3b7b5683a382df5a6ca
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2025-05-23 11:43:53 +0300

    change image format to webp

--- controllers/stake/get.js
+++ controllers/stake/get.js
@@ -10,7 +10,7 @@ module.exports = (req, res) => {
       meta: {
         title: res.__('For you to make most of the distributed value'),
         description: res.__('World is changing, and there are a lot of new values emerging with blockchain technology. Now, it is the time to distribute the value. node101 is validating and distributing the value in more than 15 blockchains, all with different visions for their community. To discover and learn more how you can join the distributed ecosystem, reach out at hello@node101.io.'),
-        image: '/img/meta/header.png',
+        image: '/img/meta/header.webp',
         twitter: true
       }
     }
